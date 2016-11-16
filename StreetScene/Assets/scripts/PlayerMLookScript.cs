02aa0a3141a2746c735cc71be5c2123627a9c476
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2016-11-16 16:18:53 +0000

    Messed adround with some teleportation but in the end scrapped it for now, also made a back up character controller, should I need it.

---
+++ StreetScene/Assets/scripts/PlayerMLookScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMLookScript : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+
+    [SerializeField]
+    private float sensitivity = 5.0f;
+    [SerializeField]
+    private float smoothing = 2.0f;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        Vector2 md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+    }
+}
---
+++ StreetScene/Assets/scripts/PlayerMovementScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float movement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(straffe, 0, movement);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
