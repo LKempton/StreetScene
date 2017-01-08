b45834f05c2fad30910de6e3a0214d8f14c25451
Lee Kempton <lee.kempton@students.plymouth.ac.uk>
2017-01-08 19:29:15 +0000

    Finshed main menu, added fonts, added menu music and created camera pan animation.

---
+++ StreetScene/Assets/scripts/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    private bool isOpen = false;
+
+    [SerializeField]
+    private GameObject controlPanel;
+
+    public void StartLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ControlsPanel()
+    {
+        if (isOpen == false)
+        {
+            controlPanel.SetActive(true);
+            isOpen = true;
+        }
+        else if (isOpen == true)
+        {
+            controlPanel.SetActive(false);
+            isOpen = false;
+        }
+    }
+}
